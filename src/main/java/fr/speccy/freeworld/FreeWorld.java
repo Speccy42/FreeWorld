42535ce82f1c8fe661920d8f89e0a1c7e7cb830b
Nicolas M <nmontchamp@gmail.com>
2026-03-15 17:18:09 +0100

    Add PulseAPI/AZPlugin/SWM dependency

--- src/main/java/fr/speccy/freeworld/FreeWorld.java
+++ src/main/java/fr/speccy/freeworld/FreeWorld.java
@@ -1,12 +1,17 @@
 package fr.speccy.freeworld;
 
+import com.grinderwolf.swm.api.SlimePlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FreeWorld extends JavaPlugin {
+    public static FreeWorld main;
+    public static SlimePlugin slimePlugin;
 
     @Override
     public void onEnable() {
-
+        main = this;
+        slimePlugin = (SlimePlugin) Bukkit.getPluginManager().getPlugin("SlimeWorldManager");
     }
 
     @Override
