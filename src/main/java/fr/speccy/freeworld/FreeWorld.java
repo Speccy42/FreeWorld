b4c83326bd7318a4ce6fed0b8bbaf4d6678f4fe6
Nicolas M <nmontchamp@gmail.com>
2026-03-15 16:26:53 +0100

    Initial project

---
+++ src/main/java/fr/speccy/freeworld/FreeWorld.java
@@ -0,0 +1,16 @@
+package fr.speccy.freeworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FreeWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
